55133a00f927790ae7d162e0b60d862c5d42650c
Alan Ribeiro <alandson_ribeiro@hotmail.com>
2021-01-01 13:03:34 -0300

    Tentando uma bosta de login

---
+++ backend/src/helpers/send-email.ts
@@ -0,0 +1,10 @@
+import * as nodemailer from "nodemailer";
+
+const config = require('config.json');
+
+module.exports = sendEmail;
+
+async function sendEmail({ to, subject, html, from = config.emailFrom }) {
+  const transporter = nodemailer.createTransport(config.smtpOptions);
+  await transporter.sendMail({ from, to, subject, html });
+}
