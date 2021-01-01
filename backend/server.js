5f87ebadb12809e0bdd33e8d4a96c8a91c9826c9
Alan Ribeiro <alandson_ribeiro@hotmail.com>
2021-01-01 19:33:04 -0300

    First Commit with some structures

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+
+import express from 'express';
+const app = express();
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// allow cors requests from any origin and with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
+app.listen(port, () => console.log('Server listening on port ' + port));
